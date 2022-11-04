fd1aed6e31d90d5b363953734ba7fcf3315032cc
RafaelXokito <Rafael_Pereira_2000@hotmail.com>
2022-11-04 16:52:21 +0000

    Initial Commit

---
+++ TripShaper/TripShaperApp.swift
@@ -0,0 +1,17 @@
+//
+//  TripShaperApp.swift
+//  TripShaper
+//
+//  Created by Rafael Pereira on 04/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct TripShaperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
